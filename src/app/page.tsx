23c94e9b20b6fa748a1153da0351e04b6b4211b2
mark7dev <mark7dev@outlook.com>
2025-07-24 21:27:21 -0600

    test deployments to vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Testing deployments.
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
