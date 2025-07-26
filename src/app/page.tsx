4f7d8d68fb339575ec50d953d0fea83b97eeaf36
mark7dev <mark7dev@outlook.com>
2025-07-25 19:23:14 -0600

    resolve review from copilot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
             Testing update PR branches.
           </li>
         </ol>
-        <p>Add message</p>
+        <p>Welcome!</p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
